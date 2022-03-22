a9ca3594cb1852db30b1890137ad13defc7d28d5
jaymody <jaykmody@gmail.com>
2022-03-22 03:06:56 +0000

    dump changes

--- data.py
+++ data.py
@@ -32,17 +32,20 @@ def get_transforms(*, data, size):
         )
 
 
+# CPU Memory Leak Issue: https://github.com/pytorch/pytorch/issues/13246
 class TrainDataset(Dataset):
     def __init__(self, df, tokenizer, transform=None):
         super().__init__()
-        self.df = df
-        self.tokenizer = tokenizer
-        self.file_paths = df["file_path"].values
-        self.labels = df["InChI_text"].values
+        self.file_paths = np.array(df["file_path"].values)
+        self.labels = (
+            df["InChI_text"]
+            .apply(lambda x: np.array(tokenizer.text_to_sequence(x)))
+            .values
+        )
         self.transform = transform
 
     def __len__(self):
-        return len(self.df)
+        return len(self.file_paths)
 
     def __getitem__(self, idx):
         file_path = self.file_paths[idx]
@@ -52,21 +55,18 @@ class TrainDataset(Dataset):
             augmented = self.transform(image=image)
             image = augmented["image"]
         label = self.labels[idx]
-        label = self.tokenizer.text_to_sequence(label)
         label_length = len(label)
-        label_length = torch.LongTensor([label_length])
-        return image, torch.LongTensor(label), label_length
+        return image, label, label_length
 
 
 class ValidDataset(Dataset):
     def __init__(self, df, transform=None):
         super().__init__()
-        self.df = df
-        self.file_paths = df["file_path"].values
+        self.file_paths = np.array(df["file_path"].values)
         self.transform = transform
 
     def __len__(self):
-        return len(self.df)
+        return len(self.file_paths)
 
     def __getitem__(self, idx):
         file_path = self.file_paths[idx]
@@ -81,13 +81,12 @@ class ValidDataset(Dataset):
 class TestDataset(Dataset):
     def __init__(self, df, transform=None):
         super().__init__()
-        self.df = df
-        self.file_paths = df["file_path"].values
+        self.file_paths = np.array(df["file_path"].values)
         self.transform = transform
         self.fix_transform = Compose([Transpose(p=1), VerticalFlip(p=1)])
 
     def __len__(self):
-        return len(self.df)
+        return len(self.file_paths)
 
     def __getitem__(self, idx):
         file_path = self.file_paths[idx]
@@ -107,10 +106,16 @@ class BMSCollator:
         self.pad_idx = pad_idx
 
     def __call__(self, batch):
-        imgs, labels, label_lengths = [], [], []
-        for data_point in batch:
-            imgs.append(data_point[0])
-            labels.append(data_point[1])
-            label_lengths.append(data_point[2])
-        labels = pad_sequence(labels, batch_first=True, padding_value=self.pad_idx)
-        return torch.stack(imgs), labels, torch.stack(label_lengths).reshape(-1, 1)
+        imgs, labels, label_lengths = list(zip(*batch))
+
+        imgs = torch.stack(imgs)
+        labels = pad_sequence(
+            [torch.LongTensor(lbl) for lbl in labels],
+            batch_first=True,
+            padding_value=self.pad_idx,
+        )
+        label_lengths = torch.stack(
+            [torch.LongTensor([lbl_len]) for lbl_len in label_lengths]
+        ).reshape(-1, 1)
+
+        return imgs, labels, label_lengths
